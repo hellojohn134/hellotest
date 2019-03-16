1d38097b1857f48d704481359329aa042b418be7
johnny185 <31239159+johnny185@users.noreply.github.com>
2019-03-16 23:22:10 +0800

    2019.3测试

---
+++ content/src/components/ChildCont.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <p>{{msg}}</p>
+    <p>他是从{{here.pro}}来</p>
+    <p>他是从{{here.type}}来</p>
+    <p>他是从{{here.color}}来</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChildCont',
+  data () {
+    return {
+      msg: '我是子组件'
+    }
+  },
+  props:['here']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ content/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <p>{{msg}}</p>
+    <ChildCont :here='info'/>
+
+  </div>
+</template>
+
+<script>
+import ChildCont from './ChildCont'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '我是父组件',
+      info:{
+        pro:'四川',
+        type:'熊猫',
+        color:'黑白相间'
+      }
+    }
+  },
+  components:{
+    ChildCont
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
